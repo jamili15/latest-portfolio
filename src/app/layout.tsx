600eeb9cf7a9fc852473f57515567748cf5c3c7a
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-08-30 15:46:21 +0800

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Created with the help of Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-slate-900 text-white">{children}</body>
+    </html>
+  );
+}
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,78 @@
+import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
+import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import SectionHeader from "@/components/SectionHeader";
+import Card from "@/components/Card";
+import grainImage from "@/assets/images/grain.jpg";
+
+const portfolioProjects = [
+  {
+    company: "Acme Corp",
+    year: "2022",
+    title: "Dark Saas Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/4k7IdSLxh6w",
+    image: darkSaasLandingPage,
+  },
+  {
+    company: "Innovative Co",
+    year: "2021",
+    title: "Light Saas Landing Page",
+    results: [
+      { title: "Boosted sales by 20%" },
+      { title: "Expanded customer reach by 35%" },
+      { title: "Increased brand awareness by 15%" },
+    ],
+    link: "https://youtu.be/7hi5zwO75yc",
+    image: lightSaasLandingPage,
+  },
+  {
+    company: "Quantum Dynamics",
+    year: "2023",
+    title: "AI Startup Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/Z7I5uSRHMHg",
+    image: aiStartupLandingPage,
+  },
+];
+
+export const ProjectsSection = () => {
+  return (
+    <div className="text-white">
+      <SectionHeader
+        eyebrow="Real-world Results"
+        title="Feaured Projects"
+        description="See how to transformed concepts into engaging digital experience"
+      />
+      <div className="mt-10 md:mt-20 flex flex-col gap-20">
+        {portfolioProjects.map((project: any) => (
+          <Card
+            key={project.title}
+            className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 "
+          >
+            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+              <div className="lg:pb-16">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.name}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
+                <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                  {project.title}
+                </h3>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
